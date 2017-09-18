cb662e6dc4d103c778fd6505b9189b82eb014ddc
hariom-kumar-bs <hariom.kumar@bigsteptech.com>
2017-09-18 11:15:35 +0530

    test commit

---
+++ Mc/Se/registration.php
@@ -0,0 +1,25 @@
+<!--
+/**
+* Magecube
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Magecube.com license that is
+* available through the world-wide-web at this URL:
+* https://www.magecube.com/LICENSE.txt
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade this extension to newer
+* version in the future.
+*
+* @category    Magecube
+* @package     Magecube_Seint
+* @copyright   Copyright (c) 2017 Magecube (http://www.magecube.com/)
+* @license     https://www.magecube.com/LICENSE.txt
+*/
+-->
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE, 'Mc_Se', __DIR__
+);
